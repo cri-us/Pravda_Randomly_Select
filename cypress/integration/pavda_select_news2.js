b126de80978c39ff1a1c34baf1143ee0621acc0b
cri-us <albazaliunsal@gmail.com>
2021-09-20 02:20:27 +0300

    Randomly selecting elements and verifying title of selected elements

--- cypress/integration/pavda_select_news2.js
+++ cypress/integration/pavda_select_news2.js
@@ -1,14 +1,21 @@
+const homepage = require('../pageObjects/homePage.json')
 context('Select feature of pravda', () => {
     it('Randomly select news of news section', () => {
-        cy.visit('https://www.pravda.com.ua')
-        cy.wait(300)
-        cy.get('.container_sub_news_wrapper')
-         .children()
-         .eq(Math.floor(Math.random() * 122))
+        cy.visit('https://www.pravda.com.ua') //visiting the home page
+        cy.wait(600)
+        cy.get(homepage.SelectingNewsInHomePage) //selecting news container in home page 
+         .children()   
+         .eq(Math.floor(Math.random() * 44)) //randomly selecting in list 
          .click({force: true}) 
-         .each(($containerSubNewsListWrapper) => {
-        cy.get($containerSubNewsListWrapper).click()
-         // cy.title('post_title').should('eq','')
-        })
+         .each(($containerSubNewsListWrapper) => {    
+        cy.get($containerSubNewsListWrapper).click() // clicking randomly selected element
+        cy.title().as('title1')  // storing its title as "title1"
+        cy.wait(900)
+         cy.get("@title1").then((title1) => {
+            cy.title().then((title2) => {   //get the title of the current page
+              expect(title1).to.equal(title2)
+            })
+          })
+        }) 
     })
 })
--- cypress/integration/pravda_select_news.js
+++ cypress/integration/pravda_select_news.js
@@ -1,17 +1,24 @@
+const homepage = require('../pageObjects/homePage.json')
+const selectNews= require('../pageObjects/selectNews.json')
 context('Select feature of pravda', () => {
     it('Select news scenario of pravda', () => {
-        cy.visit('https://www.pravda.com.ua')
-        cy.get('.main_menu > :nth-child(2) > a').click()
-        cy.wait(300)
-        cy.get('.container_sub_news_list_wrapper')
-        .children()
-        .eq(Math.floor(Math.random() * 122))
-        .click({force: true}) 
+        cy.visit('https://www.pravda.com.ua') //visiting the home page
+        cy.get(homepage.selectingNewsSection).click()  //selecting news section in homepage
+        cy.wait(600)
+        cy.get(selectNews.newsSectionRandomly.selectNews).then((newsSelection) =>{
+         cy.get(newsSelection).children() 
+        .eq(Math.floor(Math.random() * 34)) //randomly selecting in list 
+        .click({force: true})
+        })
         .each(($containerSubNewsListWrapper) => {
-       cy.get($containerSubNewsListWrapper).click()
-       // cy.title().should('eq','')
-
-   })
-     
-})
+       cy.get($containerSubNewsListWrapper).click() // clicking randomly selected element
+       cy.title().as('title1') // storing its title as "title1"
+       cy.wait(3000)
+       cy.get("@title1").then((title1) => {
+        cy.title().then((title2) => {   //get the title of the current page
+          expect(title1).to.equal(title2)
+                })
+            })
+        })  
+    })
 })
